e3828a3395d6c963d386f34e7a31e69837bfa7ac
carcheky <carcheky@gmail.com>
2022-10-08 14:06:29 +0000

    fix:  Oct 8, 2022, 4:06 PM

--- scripts/raspi.sh
+++ scripts/raspi.sh
@@ -133,7 +133,7 @@ watcher() {
     if [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | sed 's/\// /g') | cut -f1) ]; then
       docker_start
       docker ps
-      sleep 60
+      sleep 600
     else
       raspi_update
     fi
