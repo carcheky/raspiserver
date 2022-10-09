fc8f6b67b28064afe3439f4e5cfcf531e9f3cf68
carcheky <carcheky@gmail.com>
2022-10-09 18:13:28 +0000

    fix:  Oct 9, 2022, 8:13 PM

--- scripts/raspi.sh
+++ scripts/raspi.sh
@@ -113,7 +113,7 @@ mount_hd() {
 }
 docker_run() {
   if mount_hd; then
-    docker-compose up -d --remove-orphans
+    docker-compose up -d --remove-orphans --pull
     sudo chmod 777 /var/run/docker.sock
   fi
 }
