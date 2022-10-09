b22e37973df0733677456e23ed8afd27625a14fd
carcheky <carcheky@gmail.com>
2022-10-09 22:25:21 +0000

    fix:  Oct 10, 2022, 12:25 AM

--- scripts/raspi-connect.sh
+++ scripts/raspi-connect.sh
@@ -3,14 +3,7 @@
 add_key() {
   sudo rm -fr /home/user/.ssh/known_hosts /mnt/c/Users/carch/.ssh/known_hosts
   while ! ssh -oStrictHostKeyChecking=no carcheky@cckpi.local exit &>/dev/null; do
-    sleep .1 && echo -en "\\r comprobando..."
-    sleep .1 && echo -en "\\r .comprobando.."
-    sleep .1 && echo -en "\\r ..comprobando."
-    sleep .1 && echo -en "\\r ...comprobando"
-    sleep .1 && echo -en "\\r ..comprobando."
-    sleep .1 && echo -en "\\r ..comprobando."
-    sleep .1 && echo -en "\\r .comprobando.."
-    sleep .1 && echo -en "\\r comprobando..."
+    sleep 1
   done
   key=$(cat ~/.ssh/id_rsa.pub)
   command="if [ ! -d .ssh ]; then mkdir .ssh; fi ; echo \"${key}\" > .ssh/authorized_keys"
