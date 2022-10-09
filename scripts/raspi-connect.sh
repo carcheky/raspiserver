3dca46aba86d6731d088c303178a00594b88483d
carcheky <carcheky@gmail.com>
2022-10-09 12:47:52 +0000

    fix:  Oct 9, 2022, 2:47 PM

--- scripts/raspi-connect.sh
+++ scripts/raspi-connect.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-key=$(cat ~/.ssh/id_rsa.pub)
-command="if [ ! -d .ssh ]; then mkdir .ssh; fi ; echo \"${key}\" > .ssh/authorized_keys"
+
 
 add_key() {
   while ! ssh carcheky@cckpi.local ls &>/dev/null; do
@@ -8,6 +7,9 @@ add_key() {
     sleep 1
   done
   sudo rm -fr /home/user/.ssh/known_hosts /mnt/c/Users/carch/.ssh/known_hosts
+
+  key=$(cat ~/.ssh/id_rsa.pub)
+  command="if [ ! -d .ssh ]; then mkdir .ssh; fi ; echo \"${key}\" > .ssh/authorized_keys"
   ssh -oStrictHostKeyChecking=no carcheky@cckpi.local "if [ ! -d .ssh ]; then mkdir .ssh; fi ; echo \"${key}\" > .ssh/authorized_keys"
 }
 raspiserver_install(){
