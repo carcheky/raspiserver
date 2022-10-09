f8b32f1b6b86623a014b27d6b7da8dab57997586
carcheky <carcheky@gmail.com>
2022-10-09 11:32:40 +0000

    fix:  Oct 9, 2022, 1:32 PM

--- scripts/run-in-pi.sh
+++ scripts/run-in-pi.sh
@@ -3,3 +3,4 @@ sudo rm -fr /home/user/.ssh/known_hosts /mnt/c/Users/carch/.ssh/known_hosts
 key=$(cat ~/.ssh/id_rsa.pub)
 command="rm -fr .ssh && mkdir .ssh && echo \"${key}\" > .ssh/authorized_keys"
 ssh -oStrictHostKeyChecking=no carcheky@cckpi.local $command
+while true; do ssh carcheky@cckpi.local; done   
