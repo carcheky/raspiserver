e8c5007f5d22482c31e614ee9cbac4399a4cb47b
carcheky <carcheky@gmail.com>
2022-10-09 15:16:38 +0000

    fix:  Oct 9, 2022, 5:16 PM

--- scripts/raspi-connect.sh
+++ scripts/raspi-connect.sh
@@ -2,7 +2,7 @@
 
 add_key() {
   sudo rm -fr /home/user/.ssh/known_hosts /mnt/c/Users/carch/.ssh/known_hosts
-  while ! ssh -oStrictHostKeyChecking=no carcheky@cckpi.local exit 0 &>/dev/null ; do
+  while ! ssh -oStrictHostKeyChecking=no carcheky@cckpi.local exit 0 &>/dev/null; do
     sleep .1 && echo -en "\\r comprobando..."
     sleep .1 && echo -en "\\r .comprobando.."
     sleep .1 && echo -en "\\r ..comprobando."
@@ -10,7 +10,7 @@ add_key() {
     sleep .1 && echo -en "\\r ..comprobando."
     sleep .1 && echo -en "\\r ..comprobando."
     sleep .1 && echo -en "\\r .comprobando.."
-    sleep .1 && echo -en "\\r comprobando..."    
+    sleep .1 && echo -en "\\r comprobando..."
   done
   key=$(cat ~/.ssh/id_rsa.pub)
   command="if [ ! -d .ssh ]; then mkdir .ssh; fi ; echo \"${key}\" > .ssh/authorized_keys"
@@ -24,5 +24,15 @@ add_key
 raspiserver_install
 
 while true; do
+  while ! ssh -oStrictHostKeyChecking=no carcheky@cckpi.local exit 0 &>/dev/null; do
+    sleep .1 && echo -en "\\r conectando..."
+    sleep .1 && echo -en "\\r .conectando.."
+    sleep .1 && echo -en "\\r ..conectando."
+    sleep .1 && echo -en "\\r ...conectando"
+    sleep .1 && echo -en "\\r ..conectando."
+    sleep .1 && echo -en "\\r ..conectando."
+    sleep .1 && echo -en "\\r .conectando.."
+    sleep .1 && echo -en "\\r conectando..."
+  done
   ssh carcheky@cckpi.local
 done
