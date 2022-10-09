60fdfe011c8989960947f858071681efcc4e7c77
carcheky <carcheky@gmail.com>
2022-10-09 12:09:22 +0000

    fix:  Oct 9, 2022, 2:09 PM

--- scripts/raspi-connect.sh
+++ scripts/raspi-connect.sh
@@ -6,7 +6,6 @@ add_key() {
   while ! ssh carcheky@cckpi.local ls; do
     sleep 1
   done
-  echo hola
   sudo rm -fr /home/user/.ssh/known_hosts /mnt/c/Users/carch/.ssh/known_hosts
   ssh -oStrictHostKeyChecking=no carcheky@cckpi.local $command
 }
