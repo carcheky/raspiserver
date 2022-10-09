08b93a158733cd21cb5aab05ecbd4dccd1b8832a
carcheky <carcheky@gmail.com>
2022-10-09 18:06:12 +0000

    fix:  Oct 9, 2022, 8:06 PM

--- scripts/raspi.sh
+++ scripts/raspi.sh
@@ -62,7 +62,11 @@ install() {
       lsb-release
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
   fi
+
   if [ ! -d ~/raspiserver ]; then
     git clone https://gitlab.com/carcheky/raspiserver.git ~/raspiserver
     _install_raspi_bin
