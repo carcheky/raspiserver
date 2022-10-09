821d9ba4c2eec477f1b0a5cde046d104a0b523cb
carcheky <carcheky@gmail.com>
2022-10-09 13:36:48 +0000

    fix:  Oct 9, 2022, 3:36 PM

--- scripts/raspi-connect.sh
+++ scripts/raspi-connect.sh
@@ -12,7 +12,6 @@ add_key() {
     sleep .1 && echo -en "\\r comprobando..."    
   done
   sudo rm -fr /home/user/.ssh/known_hosts /mnt/c/Users/carch/.ssh/known_hosts
-
   key=$(cat ~/.ssh/id_rsa.pub)
   command="if [ ! -d .ssh ]; then mkdir .ssh; fi ; echo \"${key}\" > .ssh/authorized_keys"
   ssh -oStrictHostKeyChecking=no carcheky@cckpi.local ${command}
