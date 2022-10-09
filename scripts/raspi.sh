aede516ce3d0fb42e9b5090eb17098b45de252a2
carcheky <carcheky@gmail.com>
2022-10-09 14:43:20 +0000

    fix:  Oct 9, 2022, 4:43 PM

--- scripts/raspi.sh
+++ scripts/raspi.sh
@@ -8,6 +8,8 @@ echo "
 ██║  ██║██║  ██║███████║██║     ██║
 ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝                                
 "
+date
+
 # update date
 sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
 
