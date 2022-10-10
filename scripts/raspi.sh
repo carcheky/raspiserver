f18190aa29380a18710f5efe45b9d8e9838e3cf2
carcheky <carcheky@gmail.com>
2022-10-10 10:06:49 +0000

    fix:  Oct 10, 2022, 12:06 PM

--- scripts/raspi.sh
+++ scripts/raspi.sh
@@ -13,7 +13,9 @@ echo "
 
 # update date
 sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+
 set -eux
+
 _install() {
   sudo dpkg --configure -a
   if [ -d /raspi ]; then
