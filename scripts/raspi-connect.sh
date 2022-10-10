3cd8f80814f4116dba3eb3b8b2828c63aaf6dc1c
carcheky <carcheky@gmail.com>
2022-10-10 11:02:21 +0000

    fix:  Oct 10, 2022, 1:02 PM

--- scripts/raspi-connect.sh
+++ scripts/raspi-connect.sh
@@ -22,10 +22,10 @@ run() {
     sleep 1
     echo -en " \\r waiting script..."
   done
-  # ssh carcheky@192.168.68.136
   echo "###################################################################################"
-  # ssh carcheky@192.168.68.136 'curl https://gitlab.com/carcheky/raspiserver/-/raw/main/scripts/raspi.sh | bash'
-  # ssh carcheky@192.168.68.136 'raspi run'
+  echo "###################################################################################"
+  echo "###################################################################################"
+  echo "###################################################################################"
   ssh carcheky@192.168.68.136 'curl https://gitlab.com/carcheky/raspiserver/-/raw/main/scripts/raspi.sh | bash'
 }
 
