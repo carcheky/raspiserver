a6c567152a747e80fd82fdbecf2b7eb86864aaff
carcheky <carcheky@gmail.com>
2022-10-10 20:11:12 +0000

    fix:  Oct 10, 2022, 10:11 PM

--- scripts/raspi-connect.sh
+++ scripts/raspi-connect.sh
@@ -5,7 +5,7 @@ add_key() {
   key=$(cat ~/.ssh/id_rsa.pub)
   command="if [ ! -d .ssh ]; then mkdir .ssh; fi ; echo \"${key}\" > .ssh/authorized_keys"
   sudo rm -fr /home/user/.ssh/known_hosts /home/user/.ssh/known_hosts.old /home/user/.ssh/authorized_keys /mnt/c/Users/carch/.ssh/known_hosts
-  while ! sshpass -p locococo ssh -oStrictHostKeyChecking=no ${USER}@${HOST} ${command}; do
+  while ! sshpass -p ${PASSWORD} ssh -oStrictHostKeyChecking=no ${USER}@${HOST} ${command}; do
     echo "waiting key..."
     sleep 1
   done
