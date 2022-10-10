83946a6d291eb3ec038280c4d8333449232b9dfd
carcheky <carcheky@gmail.com>
2022-10-10 19:01:54 +0000

    fix:  Oct 10, 2022, 9:01 PM

--- scripts/raspi-connect.sh
+++ scripts/raspi-connect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eux
-key=$(cat ~/.ssh/id_rsa.pub)
-command="if [ ! -d .ssh ]; then mkdir .ssh; fi ; echo \"${key}\" > .ssh/authorized_keys"
 
 add_key() {
+  key=$(cat ~/.ssh/id_rsa.pub)
+  command="if [ ! -d .ssh ]; then mkdir .ssh; fi ; echo \"${key}\" > .ssh/authorized_keys"
   sudo rm -fr /home/user/.ssh/known_hosts /home/user/.ssh/known_hosts.old /home/user/.ssh/authorized_keys /mnt/c/Users/carch/.ssh/known_hosts
   while ! sshpass -p locococo ssh -oStrictHostKeyChecking=no carcheky@192.168.68.136 ${command}; do
     echo "waiting key..."
@@ -35,6 +35,6 @@ fi
 
 run
 rm -fr /tmp/raspi-connect.sh
-while ! curl https://gitlab.com/carcheky/raspiserver/-/raw/main/scripts/raspi-connect.sh >/tmp/raspi-connect.sh ; do
+while ! curl https://gitlab.com/carcheky/raspiserver/-/raw/main/scripts/raspi-connect.sh >/tmp/raspi-connect.sh; do
   bash /tmp/raspi-connect.sh
 done
