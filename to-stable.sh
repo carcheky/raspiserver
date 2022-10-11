d7c9ce4cf2a67350f8e8d40725c06df07f6ff7bc
carcheky <carcheky@gmail.com>
2022-10-11 15:12:46 +0000

    fix:  Oct 11, 2022, 5:12 PM

--- to-stable.sh
+++ to-stable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-gco stable 
+gco stable
 gl --no-edit --no-ff
-git merge beta --no-edit --no-ff 
-gp 
-gco beta 
-git merge stable --no-ff --no-edit 
+git merge beta --no-edit --no-ff
+gp
+gco beta
+git merge stable --no-ff --no-edit
 gp
