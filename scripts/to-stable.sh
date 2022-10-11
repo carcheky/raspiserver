f07dec48a52e0a59643acabbcf25093b807e4949
carcheky <carcheky@gmail.com>
2022-10-11 15:16:39 +0000

    fix:  Oct 11, 2022, 5:16 PM

--- scripts/to-stable.sh
+++ scripts/to-stable.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+git checkout beta
+git merge stable --no-ff --no-edit
+git push
 git checkout stable
 git pull --no-edit --no-ff
 git merge beta --no-edit --no-ff
 git push
 git checkout beta
-git merge stable --no-ff --no-edit
-git push
+
