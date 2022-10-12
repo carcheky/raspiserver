029056dac89051ef90a2f3087cfa1033841afc3f
carcheky <carcheky@gmail.com>
2022-10-12 21:44:57 +0200

    fix: to-stable script

--- scripts/to-stable.sh
+++ scripts/to-stable.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 git checkout beta
+git pull --no-ff --no-edit 
 git merge stable --no-ff --no-edit
 git push
 git checkout stable
-git pull --no-edit --no-ff
-git merge beta --no-ff
+git pull --no-ff --no-edit 
+git merge beta --no-ff --no-edit 
 git push
 git checkout beta
 
