323c8064d25049b081e4e4a5253fd9bd74139529
carcheky <carcheky@gmail.com>
2022-10-19 18:49:46 +0200

    fix: homeassistant cert

--- duckdns/duck.sh
+++ duckdns/duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=carcheky&token=a9fb24c6-fa9a-4213-95fc-e8ef5866aa8a&ip=" | curl -k -o ~/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=carcheky&token=a9fb24c6-fa9a-4213-95fc-e8ef5866aa8a&ip=" | curl -k -o /raspi/raspiserver/duckdns/duck.log -K -
