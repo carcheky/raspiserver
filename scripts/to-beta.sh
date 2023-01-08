1c22a0f7eaa1098fdfc8e22d0c6fd0b0805c6f4b
carcheky <carcheky@gmail.com>
2023-01-08 15:31:11 +0100

    chore: to-beta
    Squashed commit of the following:
    
    commit 4a1a8317a5a9bffd63ecde8207b2e804dcab6e4c
    Author: carcheky <carcheky@gmail.com>
    Date:   Sun Jan 8 15:30:39 2023 +0100
    
        chore: to-beta
    
    commit f9fa7ee66e3c39848c338507896f224cd8a7f124
    Author: carcheky <carcheky@gmail.com>
    Date:   Sun Jan 8 15:28:32 2023 +0100
    
        chore: to-beta
    
    commit d61d5353f4c4862aacb83edd966e039405809c07
    Author: carcheky <carcheky@gmail.com>
    Date:   Sun Jan 8 15:28:00 2023 +0100
    
        chore: to-beta

--- scripts/to-beta.sh
+++ scripts/to-beta.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-git checkout alpha
-git pull --no-ff --no-edit 
+
+set -eux
+
+git checkout beta &&\
+git pull --no-ff --no-edit &&\
+echo "commit message:" &&\
+read commit_message &&\
+git merge alpha --squash -m "$commit_message" &&\
+git commit &&\
 git push
-git checkout beta
-git pull --no-ff --no-edit
-git merge alpha --squash --no-commit
-git push
+
 git branch --delete alpha
-git checkout alpha
-git push --force
+# git branch --delete alpha &&\
+# git checkout -b alpha &&\
+# git push --force --set-upstream origin alpha
