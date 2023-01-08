67513c5316b6155465e2167703ef171548cd8952
carcheky <carcheky@gmail.com>
2023-01-08 15:35:48 +0100

    chore: to-beta

--- scripts/to-beta.sh
+++ scripts/to-beta.sh
@@ -8,7 +8,6 @@ git merge alpha --squash --no-commit &&\
 git commit &&\
 git push
 
-git branch --delete alpha
 git branch --delete alpha &&\
 git checkout -b alpha &&\
 git push --force --set-upstream origin alpha
