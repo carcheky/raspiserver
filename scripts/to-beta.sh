ccb5c35f0adf6f69cadc29cb424cfd892a0acf97
carcheky <carcheky@gmail.com>
2023-03-15 23:53:24 +0100

    chore: new scripts

--- scripts/to-beta.sh
+++ scripts/to-beta.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-git checkout beta &&\
-git pull --no-ff --no-edit &&\
-git merge alpha --squash --no-commit &&\
-bash scripts/git-commit-helper.sh
+# get current branch for later
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
 
-git checkout beta &&\
-git pull --no-ff --no-edit &&\
-git merge stable --no-edit  &&\
-bash scripts/git-commit-helper.sh
+# test if current brach can be merged into beta
+if ! git merge-base --is-ancestor $currentBranch beta; then
+    echo "Current branch cannot be merged into beta"
+    exit 1
+fi
+
+# check if current brach can be merged into beta
+git checkout beta
+git pull --no-ff --no-edit 
+git merge $currentBranch --squash --no-commit
+git push
+git checkout $currentBranch
