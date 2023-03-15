fb341b2cded8a025db62e7261c2164b27d9c88a1
carcheky <carcheky@gmail.com>
2023-03-15 19:51:47 +0100

    Squashed commit of the following:
    commit 63bc52793b1cf9d790e5e28db0fff2bc24f0dfe8
    Author: carcheky <carcheky@gmail.com>
    Date:   Wed Mar 15 19:51:45 2023 +0100
    
        chore: reset alpha script

--- scripts/reset-alpha.sh
+++ scripts/reset-alpha.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git reset --hard HEAD &&\
-git checkout beta &&\
-git branch -D alpha &&\
-git checkout -b alpha &&\
+git reset --hard HEAD
+git checkout --force
+git branch -D alpha
+git checkout -b alpha
 git push --force --set-upstream origin alpha
--- scripts/to-beta.sh
+++ scripts/to-beta.sh
@@ -5,7 +5,3 @@ git pull --no-ff --no-edit &&\
 git merge alpha --squash --no-commit &&\
 git commit &&\
 git push
-
-git branch --delete alpha &&\
-git checkout -b alpha &&\
-git push --force --set-upstream origin alpha
