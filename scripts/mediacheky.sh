45a2afc3f32698e07e789d34346d370caf9d9d95
carcheky <carcheky@gmail.com>
2023-10-13 09:43:47 +0000

    feat(mediacheky): admin script

---
+++ scripts/mediacheky.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+RASPISERVER="${HOME}/mediacheky/raspiserver"
+RASPICONFIG="${HOME}/mediacheky/RASPICONFIG"
+RASPIMEDIA="${HOME}/mediacheky/RASPIMEDIA/BibliotecaMultimedia"
+[ -f ${RASPISERVER}/.env ] && . ${RASPISERVER}/.env
+
+function backup() {
+
+    ####################################
+    #
+    # Backup to NFS mount script.
+    #
+    ####################################
+
+    # What to backup.
+    backup_files="$RASPISERVER $RASPICONFIG"
+
+    # Where to backup to.
+    dest="$RASPIMEDIA"
+
+    # Create archive filename.
+    day=$(date +%A)
+    hostname=$(hostname -s)
+    archive_file="$hostname-$day.tgz"
+
+    # Print start status message.
+    echo "Backing up $backup_files to $dest/$archive_file"
+    date
+    echo
+
+    # Backup the files using tar.
+    tar czf $dest/$archive_file $backup_files
+
+    # Print end status message.
+    echo
+    echo "Backup finished"
+    date
+
+    # Long listing of files in $dest to check file sizes.
+    ls -lh $dest
+}
