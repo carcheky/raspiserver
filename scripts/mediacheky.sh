e8c12cc99e7ce41dd89d6c8959daf312fb652028
carcheky <carcheky@gmail.com>
2023-11-23 07:54:34 +0100

    fix: multiple fixes

--- scripts/mediacheky.sh
+++ scripts/mediacheky.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -eux
 RASPISERVER="/home/carcheky/mediacheky/raspiserver"
 RASPICONFIG="/home/carcheky/mediacheky/RASPICONFIG"
 RASPIMEDIA="/home/carcheky/mediacheky/RASPIMEDIA/BibliotecaMultimedia"
@@ -59,6 +59,7 @@ function install() {
     sudo cp -f ${RASPISERVER}/scripts/mediacheky.sh /usr/local/bin/mediacheky
     sudo ln -fs ${RASPISERVER}/scripts/mediacheky.sh /usr/local/bin/mediacheky-dev
     sudo chmod +x /usr/local/bin/mediacheky*
+    sudo chown -R carcheky:carcheky ${raspiserver} ${RASPICONFIG}
     ls -la /usr/local/bin/mediacheky*
 }
 
