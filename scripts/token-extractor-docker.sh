1512692b38ee8e430840ece72f8a49ad7c9ab4bf
carcheky <carcheky@gmail.com>
2024-04-21 20:57:14 +0200

    chore: local dev

--- scripts/token-extractor-docker.sh
+++ scripts/token-extractor-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash <(curl -L https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/raw/master/run_docker.sh) 
+bash <(curl -L https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/raw/master/run_docker.sh)  2>&1 | tee tokens.txt
 
 rm -f token_extractor_docker.zip
 rm -fr token_extractor_docker
