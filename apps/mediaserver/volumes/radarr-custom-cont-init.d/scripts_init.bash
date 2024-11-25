98e6d27a6cf5bcb53b318454b14b781465c0f6fc
carcheky <carcheky@gmail.com>
2024-11-25 23:16:18 +0100

    feat: arr scripts included

--- apps/mediaserver/volumes/radarr-custom-cont-init.d/scripts_init.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/with-contenv bash
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/radarr/setup.bash | bash
-cp -f /custom-cont-init.d/extended.conf /config/extended.conf
-cp -f /custom-cont-init.d/recyclarr.yaml /config/extended/recyclarr.yaml
