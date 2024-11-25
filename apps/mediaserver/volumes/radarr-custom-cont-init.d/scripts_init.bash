f8652f9b40aac491ad9f4ca5fe7c8666c573690a
carcheky <carcheky@gmail.com>
2024-11-25 15:15:46 +0100

    fix:

--- apps/mediaserver/volumes/radarr-custom-cont-init.d/scripts_init.bash
+++ apps/mediaserver/volumes/radarr-custom-cont-init.d/scripts_init.bash
@@ -1,3 +1,4 @@
 #!/usr/bin/with-contenv bash
-cp -f /custom-cont-init.d/extended.conf /config/extended.conf
 curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/radarr/setup.bash | bash
+cp -f /custom-cont-init.d/extended.conf /config/extended.conf
+cp -f /custom-cont-init.d/recyclarr.yaml /config/extended/recyclarr.yaml
--- apps/mediaserver/volumes/sonarr-custom-cont-init.d/scripts_init.bash
+++ apps/mediaserver/volumes/sonarr-custom-cont-init.d/scripts_init.bash
@@ -1,3 +1,5 @@
 #!/usr/bin/with-contenv bash
-cp -f /custom-cont-init.d/extended.conf /config/extended.conf
 curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/setup.bash | bash
+cp -f /custom-cont-init.d/extended.conf /config/extended.conf
+cp -f /custom-cont-init.d/recyclarr.yaml /config/extended/recyclarr.yaml
+
