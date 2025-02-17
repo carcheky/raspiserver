166075b71dc7f3f6c1cc607237ad1a756be19130
carcheky <carcheky@gmail.com>
2025-02-17 13:34:06 +0100

    refactor: some overlay improvements

--- apps/mediaserver/jellyfin-scripts/z-lang-overlay.bash
+++ apps/mediaserver/jellyfin-scripts/z-lang-overlay.bash
@@ -2,13 +2,15 @@
 
 # Directorios: asegúrate de que estas rutas estén bien configuradas.
 OVERLAY_DIR="/BibliotecaMultimedia/flags/4x3"
-CUSTOM_CREATOR_TOOL=235334
-CUSTOM_CREATOR_TOOL=$(date)
+CUSTOM_CREATOR_TOOL=carcheky
+# CUSTOM_CREATOR_TOOL=$(date)
+flag_width=400  # Ajusta según lo necesites
+flag_height=300 # Ajusta según lo necesites
 
 # Dependencias
 function install_deps() {
     # Lista de paquetes a verificar
-    PACKAGES=("libimage-exiftool-perl" "jq" "ffmpeg" "imagemagick" "inotify-tools")
+    PACKAGES=("libimage-exiftool-perl" "jq" "ffmpeg" "imagemagick" "inotify-tools" "inkscape" "librsvg2-bin")
 
     # Filtra solo los paquetes que faltan
     MISSING_PACKAGES=()
@@ -32,38 +34,53 @@ function add_overlay() {
         local type="$2"
         offset_x=0
         offset_y=0
-        increment_x=400 # Ajusta según lo necesites
-        increment_y=300   # Ajusta según lo necesites
-        gravity="SouthWest"
-        resize=1920x2880
-        if [ "$type" == "serie" ]; then
-            gravity="South"
-            resize=1920x1080
-        fi
-        for flag_file in "${flag_files[@]}"; do
-            if [ -f "$OVERLAY_DIR/$flag_file" ]; then
-                if [ -f "$final_image" ]; then
+        if [ -f "$final_image" ]; then
+
+            dimensions=$(identify -format "%wx%h" "$final_image")
+            width=$(echo $dimensions | cut -d 'x' -f 1)
+            height=$(echo $dimensions | cut -d 'x' -f 2)
+
+            # Verificar si es horizontal o vertical
+            if [ "$width" -gt "$height" ]; then
+                # Imagen horizontal
+                gravity="SouthEast"
+                resize=2560x1440
+                offset_x=100
+            else
+                # Imagen vertical
+                gravity="SouthWest"
+                resize=1920x2880
+            fi
+
+            # Verificar si es thumb o folder.jpg
+            if [ "$type" == "thumb" ]; then
+                gravity="SouthWest"
+                offset_x=150
+            fi
+
+            for flag_file in "${flag_files[@]}"; do
+                if [ -f "$OVERLAY_DIR/$flag_file" ]; then
                     convert "$final_image" -resize ${resize}^ -gravity center -extent ${resize} tmp_thumb && mv tmp_thumb "$final_image"
 
                     convert "$final_image" \
-                        \( -density $increment_x "$OVERLAY_DIR/$flag_file" -resize "${increment_x}x${increment_y}" \) \
+                        \( -density $flag_width "$OVERLAY_DIR/$flag_file" -resize "${flag_width}x${flag_height}" \) \
                         -gravity ${gravity} -geometry +${offset_x}+${offset_y} -composite \
                         "$final_image"
 
                     chmod 644 "$final_image"
                     chown nobody "$final_image"
                     [ -f folder.jpg_exiftool_tmp ] && rm folder.jpg_exiftool_tmp -f
-                    echo "-> añadiendo $flag_file a $(pwd)/$final_image (offset: ${offset_x}x${offset_y})" && exiftool -creatortool="$CUSTOM_CREATOR_TOOL" -overwrite_original "$final_image" 1>/dev/null
+                    echo "-> añadiendo $flag_file a $(pwd)/$final_image" && exiftool -creatortool="$CUSTOM_CREATOR_TOOL" -overwrite_original "$final_image" 1>/dev/null
                     # exiftool -creatortool="$CUSTOM_CREATOR_TOOL" -overwrite_original "$final_image" -v
-                        # offset_x=$((offset_x + increment_x))
-                    if [[ "$type" == "serie" ]]; then
-                        offset_x=$((offset_x + increment_x))
+                    # offset_x=$((offset_x + flag_width))
+                    if [[ "$resize" == "2560x1440" ]]; then
+                        offset_x=$((offset_x + flag_width))
                     else
-                        offset_y=$((offset_y + increment_y))
+                        offset_y=$((offset_y + flag_height))
                     fi
                 fi
-            fi
-        done
+            done
+        fi
     fi
 }
 
@@ -84,7 +101,21 @@ function get_languages() {
         ["ita"]="it.svg"
         ["por"]="pt.svg"
         ["jpn"]="jp.svg"
+        ["ara"]="ae.svg" # Árabe
+        ["rus"]="ru.svg" # Ruso
+        ["chi"]="cn.svg" # Chino
+        ["kor"]="kr.svg" # Coreano
+        ["dut"]="nl.svg" # Neerlandés
+        ["pol"]="pl.svg" # Polaco
+        ["swe"]="se.svg" # Sueco
+        ["fin"]="fi.svg" # Finés
+        ["nor"]="no.svg" # Noruego
+        ["dan"]="dk.svg" # Danés
+        ["tur"]="tr.svg" # Turco
+        ["hin"]="in.svg" # Hindi
+        ["bel"]="be.svg" # Belga
     )
+
     flag_files=()
 
     for lang in "${langs[@]}"; do
@@ -108,7 +139,6 @@ function check_content_type() {
     echo "desconocido"
 }
 
-
 function run_on_dir() {
     cd "$OVERLAY_DIR" || exit
     cd "$MOVIES_DIR" || exit
@@ -121,8 +151,9 @@ function run_on_dir() {
             get_languages "$mlink"
             creatortool=$(exiftool -f -s3 -"creatortool" folder.jpg)
             add_overlay folder.jpg
-            # for file in *.jpg; do
-            # done
+            add_overlay backdrop.jpg
+            add_overlay poster.jpg
+            add_overlay landscape.jpg
 
         elif [[ "$(check_content_type)" == "serie" ]]; then
             echo "SERIE: $dir"
@@ -134,8 +165,11 @@ function run_on_dir() {
                 get_languages "$chapter"
                 thumb_file="${chapter%.mkv}-thumb.jpg"
                 creatortool=$(exiftool -f -s3 -"creatortool" "$thumb_file")
-                if [ "${creatortool}" != "345" ]; then
-                    add_overlay "$thumb_file" serie
+                # echo $chapter
+                # echo $thumb_file
+
+                if [ "${creatortool}" != "$CUSTOM_CREATOR_TOOL" ]; then
+                    add_overlay "$thumb_file" thumb
                 fi
             done
 
@@ -164,25 +198,28 @@ function full_logic() {
     sleep 180
 
     MOVIES_DIR="/BibliotecaMultimedia/se-borraran"
-    # run_on_dir
-    watch_folder $MOVIES_DIR &
+    run_on_dir
+    # watch_folder $MOVIES_DIR &
 
     MOVIES_DIR="/BibliotecaMultimedia/Peliculas/"
-    # run_on_dir
-    watch_folder $MOVIES_DIR &
+    run_on_dir
+    # watch_folder $MOVIES_DIR &
 
     MOVIES_DIR="/BibliotecaMultimedia/Series"
-    # run_on_dir
-    watch_folder $MOVIES_DIR &
+    run_on_dir
+    # watch_folder $MOVIES_DIR &
 
 }
 
-
 resize_all_flags() {
-    for flag_file in "$OVERLAY_DIR/*.svg"; do
-        convert -verbose "$flag_file" -resize 400x300! "$flag_file" 
-            # echo  "$OVERLAY_DIR/$flag_file" -resize 400x300^ "$OVERLAY_DIR/$flag_file"
-    done
+    if [ ! -f $OVERLAY_DIR/resized ]; then
+        for flag_file in "$OVERLAY_DIR/*.svg"; do
+            if [ "${creatortool}" != "$CUSTOM_CREATOR_TOOL" ]; then
+                convert -verbose "$flag_file" -resize ${flag_width}x${flag_height}! "$flag_file"
+            fi
+        done
+        touch $OVERLAY_DIR/resized
+    fi
 }
 
 install_deps
@@ -190,12 +227,11 @@ install_deps
 function test() {
     resize_all_flags
 
-
     MOVIES_DIR="/BibliotecaMultimedia/se-borraran"
     run_on_dir
     # watch_folder $MOVIES_DIR
 }
 
-test
+# test
 
-# full_logic &
+full_logic &
