4cf5e98b4354a2c71be9ec55adfcfab8f803b7da
carcheky <carcheky@gmail.com>
2025-04-20 23:03:11 +0200

    feat: enhance lang-flags script with verbose mode and improved argument parsing

--- apps/mediaserver/flags/lang-flags.bash
+++ apps/mediaserver/flags/lang-flags.bash
@@ -8,9 +8,11 @@ DEBUG=false # Default debug mode is off
 # Parse arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -v|--verbose) DEBUG=true; shift ;;
-        *) break ;;
+        -v|--verbose) DEBUG=true ;;
+        all) MODE="all" ;;
+        *) ;;
     esac
+    shift
 done
 
 CUSTOM_CREATOR_TOOL=$(date)                      # Default to current date
@@ -31,7 +33,7 @@ A_BORRAR_DIR="/BibliotecaMultimedia/se-borraran" # Directory for files to be del
 
 # Ensure the temporary directory exists
 install_deps() {
-    DEBUG=true && echo "Script is running as user: $(whoami), group: $(id -gn)" >&2
+    $DEBUG && echo "Script is running as user: $(whoami), group: $(id -gn)" >&2
     local packages=("perl-image-exiftool" "jq" "imagemagick" "ffmpeg" "inkscape" "rsvg-convert" "exiftool")
     local script_dir="/custom-cont-init.d"
     local script_file="$script_dir/lang_flags-install_deps.sh"
@@ -44,7 +46,7 @@ install_deps() {
     } >"$script_file"
 
     chmod +x "$script_file"
-    DEBUG=true && echo "Dependency installation script created at $script_file" >&2
+    $DEBUG && echo "Dependency installation script created at $script_file" >&2
 }
 
 # Function to extract languages from the movie file using ffprobe
@@ -53,7 +55,7 @@ get_languages() {
     if [ -f "$video_file" ]; then
         mapfile -t langs < <(ffprobe "$video_file" -show_entries stream_tags=language -select_streams a -v 0 -of json | jq --raw-output '.streams[].tags.language // empty' | sort -u)
     else
-        DEBUG=true && echo "Error: Video file $video_file not found." >&2
+        $DEBUG && echo "Error: Video file $video_file not found." >&2
         langs=()
     fi
     declare -A map
@@ -87,7 +89,7 @@ get_languages() {
 
 # Function to apply the overlay on the image (thumb or folder.jpg)
 add_overlay() {
-    DEBUG=true && echo "Starting add_overlay for image: $1, type: $2" >&2
+    $DEBUG && echo "Starting add_overlay for image: $1, type: $2" >&2
 
     local final_image="$1"
     local type="$2"
@@ -95,22 +97,22 @@ add_overlay() {
     # Generate creatortool using exiftool
     creatortool=$(exiftool -f -s3 -"creatortool" "$final_image")
     if [ -z "$creatortool" ]; then
-        DEBUG=true && echo "Warning: creatortool has no value for image: $final_image" >&2
+        $DEBUG && echo "Warning: creatortool has no value for image: $final_image" >&2
     fi
-    DEBUG=true && echo "Debug: CUSTOM_CREATOR_TOOL is set to: $CUSTOM_CREATOR_TOOL" >&2
-    DEBUG=true && echo "Debug: creatortool is set to: $creatortool" >&2
+    $DEBUG && echo "Debug: CUSTOM_CREATOR_TOOL is set to: $CUSTOM_CREATOR_TOOL" >&2
+    $DEBUG && echo "Debug: creatortool is set to: $creatortool" >&2
 
-    DEBUG=true && echo "Checking if creatortool matches CUSTOM_CREATOR_TOOL..." >&2
+    $DEBUG && echo "Checking if creatortool matches CUSTOM_CREATOR_TOOL..." >&2
     if [ "${creatortool}" != "$CUSTOM_CREATOR_TOOL" ]; then
-        DEBUG=true && echo "creatortool does not match CUSTOM_CREATOR_TOOL. Proceeding with overlay application." >&2
+        $DEBUG && echo "creatortool does not match CUSTOM_CREATOR_TOOL. Proceeding with overlay application." >&2
         offset_x=0
         offset_y=0
         if [ -f "$final_image" ]; then
-            DEBUG=true && echo "Processing image: $final_image" >&2
+            $DEBUG && echo "Processing image: $final_image" >&2
 
             dimensions=$(identify -format "%wx%h" "$final_image" 2>/dev/null)
             if [ -z "$dimensions" ]; then
-                DEBUG=true && echo "Error: Unable to retrieve dimensions for $final_image. Skipping." >&2
+                $DEBUG && echo "Error: Unable to retrieve dimensions for $final_image. Skipping." >&2
                 return
             fi
 
@@ -118,27 +120,27 @@ add_overlay() {
             height=$(echo $dimensions | cut -d 'x' -f 2)
 
             if ! [[ "$width" =~ ^[0-9]+$ ]] || ! [[ "$height" =~ ^[0-9]+$ ]]; then
-                DEBUG=true && echo "Error: Invalid dimensions ($dimensions) for $final_image. Skipping." >&2
+                $DEBUG && echo "Error: Invalid dimensions ($dimensions) for $final_image. Skipping." >&2
                 return
             fi
 
-            DEBUG=true && echo "Image dimensions: ${width}x${height}" >&2
+            $DEBUG && echo "Image dimensions: ${width}x${height}" >&2
 
             # Check if the image is horizontal or vertical
             if [ "$width" -gt "$height" ]; then
-                DEBUG=true && echo "Image is horizontal." >&2
+                $DEBUG && echo "Image is horizontal." >&2
                 gravity="SouthEast"
                 resize=$poster_resize
                 offset_x=100
             else
-                DEBUG=true && echo "Image is vertical." >&2
+                $DEBUG && echo "Image is vertical." >&2
                 gravity="SouthWest"
                 resize=$vertical_resize
             fi
 
             # Check if it is a thumb or folder.jpg
             if [ "$type" == "thumb" ]; then
-                DEBUG=true && echo "Image type is thumb." >&2
+                $DEBUG && echo "Image type is thumb." >&2
                 gravity="SouthWest"
                 offset_x=150
             fi
@@ -148,22 +150,21 @@ add_overlay() {
 
             for flag_file in "${flag_files[@]}"; do
                 if [ -f "$OVERLAY_DIR/$flag_file" ]; then
-                    DEBUG=true && echo "Adding flag: $flag_file to image: $final_image" >&2
+                    $DEBUG && echo "Adding flag: $flag_file to image: $final_image" >&2
 
                     magick "$final_image" \
                         \( -density $flag_width "$OVERLAY_DIR/$flag_file" -resize "${flag_width}x${flag_height}" \) \
                         -gravity ${gravity} -geometry +${offset_x}+${offset_y} -composite \
                         "$final_image"
 
-                    chmod 644 "$final_image" || DEBUG=true && echo "Warning: Unable to change permissions for $final_image" >&2
-                    chown nobody "$final_image" 2>/dev/null || DEBUG=true && echo "Warning: Unable to change ownership for $final_image" >&2
+                    chown nobody "$final_image" 2>/dev/null || $DEBUG && echo "Warning: Unable to change ownership for $final_image" >&2
                     [ -f folder.jpg_exiftool_tmp ] && rm folder.jpg_exiftool_tmp -f
-                    DEBUG=true && echo "-> Added $flag_file to $(pwd)/$final_image" >&2
+                    $DEBUG && echo "-> Added $flag_file to $(pwd)/$final_image" >&2
 
                     if command -v exiftool >/dev/null 2>&1; then
                         exiftool -creatortool="$CUSTOM_CREATOR_TOOL" -overwrite_original "$final_image" 1>/dev/null
                     else
-                        DEBUG=true && echo "Error: exiftool not found. Skipping metadata update." >&2
+                        $DEBUG && echo "Error: exiftool not found. Skipping metadata update." >&2
                     fi
 
                     if [[ "$resize" == "$poster_resize" ]]; then
@@ -172,30 +173,35 @@ add_overlay() {
                         offset_y=$((offset_y + flag_height))
                     fi
                 else
-                    DEBUG=true && echo "Flag file $flag_file not found in $OVERLAY_DIR." >&2
+                    $DEBUG && echo "Flag file $flag_file not found in $OVERLAY_DIR." >&2
                 fi
             done
         else
-            DEBUG=true && echo "Image file $final_image not found." >&2
+            $DEBUG && echo "Image file $final_image not found." >&2
         fi
     else
-        DEBUG=true && echo "creatortool matches CUSTOM_CREATOR_TOOL. Skipping overlay application." >&2
+        $DEBUG && echo "creatortool matches CUSTOM_CREATOR_TOOL. Skipping overlay application." >&2
     fi
-    DEBUG=true && echo "Finished add_overlay for image: $1, type: $2" >&2
+    $DEBUG && echo "Finished add_overlay for image: $1, type: $2" >&2
 }
 
 # Function to wait for nfo and process the image
 wait_for_nfo_and_process() {
     local content_path="$1"
     local is_all_mode="$2" # Pass "true" if running in "all" mode
-    DEBUG=true && echo "Checking for movie.nfo or tvshow.nfo in $content_path..." >&2
+
+    echo "=======================" >&2
+    echo "Processing folder: $content_path" >&2
+    echo "=======================" >&2
+
+    $DEBUG && echo "Checking for movie.nfo or tvshow.nfo in $content_path..." >&2
 
     # Check if there are no .mkv files in the folder
     if ! find "$content_path" -maxdepth 1 -type f -name '*.mkv' | grep -q .; then
-        DEBUG=true && echo "========================================" >&2
-        DEBUG=true && echo "WARNING: No .mkv file found in $content_path." >&2
-        DEBUG=true && echo "THIS FOLDER WILL BE MOVED TO $A_BORRAR_DIR." >&2
-        DEBUG=true && echo "========================================" >&2
+        $DEBUG && echo "========================================" >&2
+        $DEBUG && echo "WARNING: No .mkv file found in $content_path." >&2
+        $DEBUG && echo "THIS FOLDER WILL BE MOVED TO $A_BORRAR_DIR." >&2
+        $DEBUG && echo "========================================" >&2
 
         mkdir -p "$A_BORRAR_DIR"
         mv "$content_path" "$A_BORRAR_DIR/"
@@ -208,16 +214,16 @@ wait_for_nfo_and_process() {
         sleep 1
         elapsed=$((elapsed + 1))
         if [ "$elapsed" -ge "$timeout" ]; then
-            DEBUG=true && echo "Timeout reached while waiting for .nfo files in $content_path." >&2
+            $DEBUG && echo "Timeout reached while waiting for .nfo files in $content_path." >&2
             return
         fi
     done
 
     if [ -f "$content_path/movie.nfo" ]; then
-        DEBUG=true && echo "movie.nfo found in $content_path! Identified as a movie." >&2
+        $DEBUG && echo "movie.nfo found in $content_path! Identified as a movie." >&2
         local mkv_file="${radarr_moviefile_path:-$(find "$content_path" -maxdepth 1 -type f -name '*.mkv' | head -n 1)}"
         if [ -z "$mkv_file" ] || [ ! -f "$mkv_file" ]; then
-            DEBUG=true && echo "Error: No valid .mkv file found for the movie in $content_path." >&2
+            $DEBUG && echo "Error: No valid .mkv file found for the movie in $content_path." >&2
             return
         fi
 
@@ -225,17 +231,17 @@ wait_for_nfo_and_process() {
 
         if [ "$is_all_mode" == "true" ]; then
             # Process folder.jpg and backdrop.jpg if they exist, without waiting
-            [ -f "$content_path/folder.jpg" ] && add_overlay "$content_path/folder.jpg" "folder" || DEBUG=true && echo "Skipping: folder.jpg not found in $content_path." >&2
-            [ -f "$content_path/backdrop.jpg" ] && add_overlay "$content_path/backdrop.jpg" "backdrop" || DEBUG=true && echo "Skipping: backdrop.jpg not found in $content_path." >&2
+            [ -f "$content_path/folder.jpg" ] && add_overlay "$content_path/folder.jpg" "folder" || $DEBUG && echo "Skipping: folder.jpg not found in $content_path." >&2
+            [ -f "$content_path/backdrop.jpg" ] && add_overlay "$content_path/backdrop.jpg" "backdrop" || $DEBUG && echo "Skipping: backdrop.jpg not found in $content_path." >&2
         else
             # Wait for folder.jpg and backdrop.jpg with a timeout
             elapsed=0
             while [ ! -f "$content_path/folder.jpg" ] || [ ! -f "$content_path/backdrop.jpg" ]; do
-                DEBUG=true && echo "Waiting for folder.jpg and backdrop.jpg in $content_path..." >&2
+                $DEBUG && echo "Waiting for folder.jpg and backdrop.jpg in $content_path..." >&2
                 sleep 1
                 elapsed=$((elapsed + 1))
                 if [ "$elapsed" -ge "$timeout" ]; then
-                    DEBUG=true && echo "Timeout reached while waiting for folder.jpg or backdrop.jpg in $content_path." >&2
+                    $DEBUG && echo "Timeout reached while waiting for folder.jpg or backdrop.jpg in $content_path." >&2
                     return
                 fi
             done
@@ -243,63 +249,64 @@ wait_for_nfo_and_process() {
             if [ -f "$content_path/folder.jpg" ]; then
                 add_overlay "$content_path/folder.jpg" "folder"
             else
-                DEBUG=true && echo "Error: folder.jpg not found in $content_path." >&2
+                $DEBUG && echo "Error: folder.jpg not found in $content_path." >&2
             fi
 
             if [ -f "$content_path/backdrop.jpg" ]; then
                 add_overlay "$content_path/backdrop.jpg" "backdrop"
             else
-                DEBUG=true && echo "Error: backdrop.jpg not found in $content_path." >&2
+                $DEBUG && echo "Error: backdrop.jpg not found in $content_path." >&2
             fi
         fi
     elif [ -f "$content_path/tvshow.nfo" ]; then
-        DEBUG=true && echo "tvshow.nfo found in $content_path! Identified as a series." >&2
+        $DEBUG && echo "tvshow.nfo found in $content_path! Identified as a series." >&2
         # Add series-specific processing logic here if needed
+    else
+        $DEBUG && echo "No movie.nfo or tvshow.nfo found in $content_path. Skipping." >&2
     fi
 }
 
 # Function to process all movies or series in a base directory
 process_all() {
-    local movies_dir="$1"
-    local series_dir="$2"
-    DEBUG=true && echo "Processing all movies in $movies_dir and all series in $series_dir..." >&2
+    echo "Processing all movies in $MOVIES_DIR and all series in $SERIES_DIR..." >&2
+    $DEBUG && echo "Starting batch processing..." >&2
 
     # Process movies
-    for dir in "$movies_dir"/*/; do
+    for dir in "$MOVIES_DIR"/*/; do
         if [ -d "$dir" ]; then
-            DEBUG=true && echo "Processing movie directory: $dir" >&2
+            $DEBUG && echo "Processing movie directory: $dir" >&2
             wait_for_nfo_and_process "$dir" "true"
         fi
     done
 
     # Process series
-    # for dir in "$series_dir"/*/; do
+    # for dir in "$SERIES_DIR"/*/; do
     #     if [ -d "$dir" ]; then
-    #         DEBUG=true && echo "Processing series directory: $dir" >&2
+    #         $DEBUG && echo "Processing series directory: $dir" >&2
     #         wait_for_nfo_and_process "$dir" "true"
     #     fi
     # done
 
     wait # Wait for all background processes to finish
-    DEBUG=true && echo "Finished processing all directories in $movies_dir and $series_dir." >&2
+    $DEBUG && echo "Finished batch processing." >&2
 }
 
 # Function to handle Radarr or Sonarr events
 process_radarr_sonarr_event() {
     if [ -n "$radarr_eventtype" ]; then
-        DEBUG=true && echo "Source: Radarr" >&2
-        DEBUG=true && echo "Radarr Event Type: $radarr_eventtype" >&2
+        $DEBUG && echo "Source: Radarr" >&2
+        $DEBUG && echo "Radarr Event Type: $radarr_eventtype" >&2
         if [ -n "$radarr_movie_path" ]; then
             wait_for_nfo_and_process "$radarr_movie_path" "false"
         fi
     elif [ -n "$sonarr_eventtype" ]; then
-        DEBUG=true && echo "Source: Sonarr" >&2
-        DEBUG=true && echo "Sonarr Event Type: $sonarr_eventtype" >&2
+        $DEBUG && echo "Source: Sonarr" >&2
+        $DEBUG && echo "Sonarr Event Type: $sonarr_eventtype" >&2
         if [ -n "$sonarr_series_path" ]; then
             wait_for_nfo_and_process "$sonarr_series_path" "false"
         fi
     else
-        DEBUG=true && echo "Error: Neither Radarr nor Sonarr event detected." >&2
+        echo "Error: Neither Radarr nor Sonarr event detected." >&2
         exit 1
     fi
 }
@@ -309,11 +316,11 @@ cleanup_jellyfin_cache() {
     local cache_dirs=("/jellyfin-config/cache" "/jellyfin-config/.cache")
     for dir in "${cache_dirs[@]}"; do
         if [ -d "$dir" ]; then
-            DEBUG=true && echo "Deleting cache directory: $dir" >&2
+            $DEBUG && echo "Deleting cache directory: $dir" >&2
             rm -rf "$dir"
-            DEBUG=true && echo "Cache directory $dir deleted." >&2
+            $DEBUG && echo "Cache directory $dir deleted." >&2
         else
-            DEBUG=true && echo "Cache directory $dir does not exist. Skipping." >&2
+            $DEBUG && echo "Cache directory $dir does not exist. Skipping." >&2
         fi
     done
 }
@@ -331,10 +338,10 @@ install_deps
 # Print the number of instances of this script currently running
 script_name=$(basename "$0")
 instance_count=$(pgrep -fc "$script_name")
-DEBUG=true && echo "Number of instances of $script_name running: $instance_count" >&2
+$DEBUG && echo "Number of instances of $script_name running: $instance_count" >&2
 
 # Main logic
-if [ "$1" == "all" ]; then
+if [ "$MODE" == "all" ]; then
     process_all "$MOVIES_DIR" "$SERIES_DIR"
     cleanup_jellyfin_cache
 else
@@ -343,5 +350,5 @@ else
 fi
 
 # Log successful execution to stdout
-DEBUG=true && echo "Script executed successfully." >&1
+echo "Script executed successfully." >&1
 echo $SECONDS
