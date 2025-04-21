d7e767f64454098a15f36d5ddc7db41516ba8d3d
carcheky <carcheky@gmail.com>
2025-04-21 20:36:58 +0200

    fix: ensure proper script termination by adding exit command in scripts_init.bash

--- apps/mediaserver/radarr-arr-scripts/scripts_init.bash
+++ apps/mediaserver/radarr-arr-scripts/scripts_init.bash
@@ -2,3 +2,4 @@
 curl https://raw.githubusercontent.com/carcheky/arr-scripts/main/radarr/setup.bash | bash
 cp -f /custom-cont-init.d/conf/extended.conf /config/extended.conf
 cp -f /custom-cont-init.d/conf/recyclarr.yaml /config/extended/recyclarr.yaml
+exit
